21cb392101c0b60be034425303d7fb69184d835a
jud3v <minipacha972@outlook.fr>
2020-09-15 18:02:33 +0200

    adding accessibility

--- script/app.js
+++ script/app.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector('.btn');
-
-btn.addEventListener('click', () => {
-    btn.classList.toggle('active');
-})
+if (btn != null){
+    btn.addEventListener('click', () => {
+        btn.classList.toggle('active');
+    })
+}
