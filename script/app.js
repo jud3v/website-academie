c43709949654d6419bf0f24d67893cbc7343bf61
jud3v <minipacha972@outlook.fr>
2020-09-17 16:25:13 +0200

    adding features and removing features

--- script/app.js
+++ script/app.js
@@ -8,3 +8,25 @@ hamburger.addEventListener('click', () => {
       link.classList.toggle('fade')
    })
 });
+
+(() => {
+   setTimeout(() => {
+      switch (window.location.pathname){
+         case '/website-academie/index.html':
+         case '/website-academie/':
+            document.querySelector('.spy').classList.toggle('active');
+            break;
+         case '/website-academie/pages/players.html':
+            document.querySelector('.spy').classList.toggle('active');
+            break;
+         case '/website-academie/pages/about.html':
+            document.querySelector('.spy').classList.toggle('active');
+            break;
+         case '/website-academie/pages/contact.html':
+            document.querySelector('.spy').classList.toggle('active');
+            break;
+         default:
+            return false;
+      }
+   },250)
+})()
